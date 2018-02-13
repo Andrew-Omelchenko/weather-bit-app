da07752943bfbf081bec7f4d152f12131b603152
Andrew Omelchenko <andrewom43@gmail.com>
2018-02-13 12:32:59 +0200

    add aria-labelledby to base-units element

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,17 @@ module.exports = {
   module: {
     rules: [{
         test: /\.scss$/,
-        use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-        }, {
-            loader: "css-loader" // translates CSS into CommonJS
-        }, {
-            loader: "sass-loader" // compiles Sass to CSS
-        }]
+        use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader" [{
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        })
     }]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ]
 }
