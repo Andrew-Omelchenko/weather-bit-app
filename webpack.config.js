41307458d5cb00e26122b37344f824e324e2308b
Andrew Omelchenko <andrewom43@gmail.com>
2018-02-13 22:14:44 +0200

    rearrange html elements to reflect the logic of application on view layer

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  module: {
-    rules: [{
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: "css-loader" [{
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-        })
-    }]
-  },
-  plugins: [
-    new ExtractTextPlugin("styles.css"),
-  ]
+  // module: {
+  //   rules: [{
+  //       test: /\.scss$/,
+  //       use: ExtractTextPlugin.extract({
+  //           fallback: "style-loader",
+  //           use: "css-loader" [{
+  //               loader: "css-loader" // translates CSS into CommonJS
+  //           }, {
+  //               loader: "sass-loader" // compiles Sass to CSS
+  //           }]
+  //       })
+  //   }]
+  // },
+  // plugins: [
+  //   new ExtractTextPlugin("styles.css"),
+  // ]
 }
